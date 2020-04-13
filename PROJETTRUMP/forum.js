12c26a55f03d236d099ed12d13350f40dee29bae
AkimLanglois <60238631+AkimLanglois@users.noreply.github.com>
2020-04-13 03:22:43 -0400

    Add files via upload

---
+++ PROJETTRUMP/forum.js
@@ -0,0 +1,9 @@
+function delay(URL) {
+  setTimeout(function() {
+    window.location = URL;
+  }, 500);
+  document.getElementById("nav").className = "navOut";
+  document.getElementById("img").className = "imgOut";
+  document.getElementById("divParagraphe").className = "divParagrapheOut";
+  document.getElementById("divForum").className = "divForumOut";
+}
